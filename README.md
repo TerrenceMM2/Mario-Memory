bf303790c7f00358a84e572aabb243408acdb7e7
TerrenceMM2 <terrencemm2@gmail.com>
2019-09-05 09:37:31 -0500

    initial commit and react app structuring

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
 # coursework15-memory-game
 Vanderbilt Coding Boot Camp - Coursework 15 - React Memory Game
---
+++ src/components/Card/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./style.css";
+
+class Card extends Component {
+
+    state = {
+
+    };
+
+    render() {
+        return (
+            <h1>Hello world!</h1>
+        );
+    }
+
+}
+
+export default Card;
