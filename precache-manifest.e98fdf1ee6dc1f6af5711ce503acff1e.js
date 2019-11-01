db9ba9b04b9821fdc63ebe011432fcaaf565e197
TerrenceMM2 <terrencemm2@gmail.com>
2019-11-01 18:09:26 -0500

    Updates

---
+++ precache-manifest.e98fdf1ee6dc1f6af5711ce503acff1e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "930bdf212c020a7b5626dd306c1507d1",
+    "url": "/Mario-Memory/index.html"
+  },
+  {
+    "revision": "e301d5246dfccc47f349",
+    "url": "/Mario-Memory/static/css/main.676e558c.chunk.css"
+  },
+  {
+    "revision": "765a745004710180634c",
+    "url": "/Mario-Memory/static/js/2.e79872b9.chunk.js"
+  },
+  {
+    "revision": "e301d5246dfccc47f349",
+    "url": "/Mario-Memory/static/js/main.c8e6d460.chunk.js"
+  },
+  {
+    "revision": "8b687e041f56782508e9",
+    "url": "/Mario-Memory/static/js/runtime~main.1575c485.js"
+  },
+  {
+    "revision": "06e4237f57ca9b9f20526988890d7e94",
+    "url": "/Mario-Memory/static/media/SuperMarioGalaxy.06e4237f.woff2"
+  },
+  {
+    "revision": "6c02f15fdbc9dd7c482b52b06d8e0a6c",
+    "url": "/Mario-Memory/static/media/SuperMarioGalaxy.6c02f15f.ttf"
+  },
+  {
+    "revision": "a80cece94497167c1a37521a8870e17a",
+    "url": "/Mario-Memory/static/media/SuperMarioGalaxy.a80cece9.woff"
+  },
+  {
+    "revision": "5ce9694f97f76036c6bac92b8f178c38",
+    "url": "/Mario-Memory/static/media/background.5ce9694f.png"
+  },
+  {
+    "revision": "65c9e232b10ba638e203a6f5badfd9df",
+    "url": "/Mario-Memory/static/media/smb3.65c9e232.woff"
+  },
+  {
+    "revision": "b2963311bc4e84f558401ca8e05c879b",
+    "url": "/Mario-Memory/static/media/smb3.b2963311.ttf"
+  },
+  {
+    "revision": "d816cebf6bd2a8f0c2dd4272b426f06b",
+    "url": "/Mario-Memory/static/media/smb3.d816cebf.woff2"
+  }
+]);
